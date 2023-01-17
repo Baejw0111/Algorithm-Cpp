05bc21a08d5252a9cb8b7a8ed7291f3d70ec5daa
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-01-17 11:11:25 +0900

    Modify BOJ 17413

--- 백준 알고리즘 강의/알고리즘 기초 1/1. 자료구조/201(자료구조 1 연습)/17413(단어 뒤집기 2).cpp
+++ 백준 알고리즘 강의/알고리즘 기초 1/1. 자료구조/201(자료구조 1 연습)/17413(단어 뒤집기 2).cpp
@@ -1,116 +1,63 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-class Stack{
-    struct node{
-        T num;
-        node* next;
-    };
-    using link=node*;
+// 단어 전용 스택
+stack<char> word;
+// 꺽쇠 기호 존재 상태
+int bracket = 0;
 
-    link top;
+// word를 비우면서 출력하는 함수
+void clear()
+{
+    while (!word.empty())
+    {
+        cout << word.top();
+        word.pop();
+    }
+}
 
-public:
-    Stack():top(NULL){}
-    void Push(T x);
-    T Pop();
-    int Empty();
-    ~Stack();
-};
-
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    //c++의 표준 stream의 동기화를 끊는 역할을 하여 입출력의 속도를 높인다.
+    // c++의 표준 stream의 동기화를 끊는 역할을 하여 입출력의 속도를 높인다.
 
-    string str;
-    getline(cin,str);
+    string S;
 
-    Stack <char>stk1,stk2;
+    getline(cin, S);
 
-    int start=0;
-    for(int i=0;i<str.length();i++){
-        if(str[i]=='<'){
-            stk2.Push(str[i]);
-            for(int k=start;k<i;k++){
-                str[k]=stk1.Pop();
+    for (char a : S)
+    {
+        if (!bracket)
+        {
+            if (a == ' ' || a == '<')
+            {
+                clear();
+                cout << a;
             }
-            start=i+1;
-        }
-        else if(str[i]=='>'){
-            stk2.Pop();
-            start=i+1;
-        }
-
-        if(stk2.Empty()){
-            if(str[i]==' '){
-                for(int k=start;k<i;k++){
-                    str[k]=stk1.Pop();
-                }
-                start=i+1;
-            }
-            else{
-                stk1.Push(str[i]);
+            else
+            {
+                word.push(a);
             }
         }
-        else{
+        else
+        {
+            cout << a;
+        }
+
+        if (a == '<')
+        {
+            bracket = 1;
             continue;
         }
-    }
-
-    if(str[str.length()-1]!='>'){
-        for(int i=start;i<str.length();i++){
-            str[i]=stk1.Pop();
+        else if (a == '>')
+        {
+            bracket = 0;
         }
     }
 
-    cout<<str<<'\n';
+    clear();
 
     return 0;
-}
-
-
-template <typename T>
-void Stack<T>::Push(T x){
-    if(top==NULL){
-        top=new node;
-        top->num=x;
-        top->next=NULL;
-    }
-    else{
-        link tmp=new node;
-        tmp->num=x;
-        tmp->next=top;
-        top=tmp;
-    }
-}
-
-template <typename T>
-T Stack<T>::Pop(){
-    int tmp=top->num;
-    top=top->next;
-
-    return tmp;
-}
-
-template <typename T>
-int Stack<T>::Empty(){
-    if(top==NULL){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-
-template <typename T>
-Stack<T>::~Stack(){
-    while(top){
-        link tmp=top;
-        top=top->next;
-        delete tmp;
-    }
 }
