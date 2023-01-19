7469044949d608239f1c522e973a016139b43876
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-01-20 08:59:55 +0900

    Modify BOJ 17298 and 10799

--- 백준 알고리즘 강의/알고리즘 기초 1/1. 자료구조/201(자료구조 1 연습)/17298(오큰수).cpp
+++ 백준 알고리즘 강의/알고리즘 기초 1/1. 자료구조/201(자료구조 1 연습)/17298(오큰수).cpp
@@ -1,94 +1,58 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Stack{
-    struct node{
-        int num;
-        node* next;
-    };
-    using link=node*;
+int n;
+/*
+arr: 수열 A를 입력받을 배열
+ans: 수열 A의 각 수의 오큰수를 저장할 배열
+*/
+vector<int> arr, ans;
 
-    link top;
-    int size;
+// 오큰수를 알아낼 때 사용할 스택
+stack<int> stk;
 
-public:
-    Stack():top(NULL),size(0){}
-    void Push(int x);
-    int Pop();
-    int Top();
-    ~Stack();
-};
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int main(){
-    int n;
-    cin>>n;
+    cin >> n;
 
-    int *arr=new int[n],*ans=new int[n];
+    arr.resize(n);
+    ans.resize(n);
 
-    Stack stk;
-
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
 
-    stk.Push(-1);
+    //오큰수가 없는 수를 고려해 코드 작성에 용이하도록 -1을 push
+    stk.push(-1);
 
-    for(int i=n-1;i>=0;i--){
-        while(stk.Top()>0 && stk.Top()<=arr[i]){
-            stk.Pop();
+    // 수열 A를 거꾸로 읽어나간다.
+    for (int i = n - 1; i >= 0; --i)
+    {
+        /*
+        현재 스캔 중인 수보다 스택의 가장 위에 있는 수(top)가 더 크면 pop
+        또는 top이 -1이면 pop
+        */
+        while (stk.top() > 0 && stk.top() <= arr[i])
+        {
+            stk.pop();
         }
-        ans[i]=stk.Top();
-        stk.Push(arr[i]);
+
+        //위 과정이 끝났을 때의 top을 오큰수로 저장
+        ans[i] = stk.top();
+
+        //현재 스캔 중인 수를 스택에 저장
+        stk.push(arr[i]);
     }
 
-    for(int i=0;i<n;i++){
-        cout<<ans[i]<<" ";
+    for (int i = 0; i < n; ++i)
+    {
+        cout << ans[i] << ' ';
     }
 
     return 0;
-}
-
-
-void Stack::Push(int x){
-    if(top==NULL){
-        top=new node;
-        top->num=x;
-        top->next=NULL;
-    }
-    else{
-        link tmp=new node;
-        tmp->num=x;
-        tmp->next=top;
-        top=tmp;
-    }
-
-    size++;
-}
-
-int Stack::Pop(){
-    if(size==0){
-        return -1;
-    }
-    else{
-        int tmp=top->num;
-        link tmpnod=top;
-        top=top->next;
-        delete tmpnod;
-        size--;
-
-        return tmp;
-    }
-}
-
-int Stack::Top(){
-    return top->num;
-}
-
-Stack::~Stack(){
-    while(top){
-        link tmp=top;
-        top=top->next;
-        delete tmp;
-    }
 }
