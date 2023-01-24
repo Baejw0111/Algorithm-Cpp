7d382aeeb5b00a8496e95002a850770075bb210b
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-01-24 14:20:11 +0900

    .

--- 백준 알고리즘 강의/알고리즘 기초 1/1. 자료구조/201(자료구조 1 연습)/17299(오등큰수).cpp
+++ 백준 알고리즘 강의/알고리즘 기초 1/1. 자료구조/201(자료구조 1 연습)/17299(오등큰수).cpp
@@ -8,7 +8,7 @@ seq: 수열 A를 저장할 스택
 stk: 오등큰수를 알아낼 때 사용할 스택
 ans: 수열 A의 각 수의 오등큰수를 저장할 스택
 */
-stack<int> seq,stk,ans;
+stack<int> seq, stk, ans;
 
 // cnt: 각 수의 등장 횟수를 저장할 벡터
 vector<int> cnt;
@@ -22,7 +22,7 @@ int main()
     cin >> n;
 
     // 수열의 최대값을 저장하기 위한 변수
-    int m=0;
+    int m = 0;
 
     for (int i = 0; i < n; i++)
     {
@@ -30,10 +30,10 @@ int main()
         cin >> tmp;
 
         seq.push(tmp);
-        m=max(m,tmp);
+        m = max(m, tmp);
         // 최대값+1의 크기로 cnt의 인덱스 범위 늘리기
-        cnt.resize(m+1);
-        cnt[tmp]+=1;
+        cnt.resize(m + 1);
+        cnt[tmp] += 1;
     }
 
     /*
@@ -43,7 +43,7 @@ int main()
     stk.push(-1);
 
     // 수열 A를 거꾸로 읽어나간다.
-    while(!seq.empty())
+    while (!seq.empty())
     {
         /*
         현재 스캔 중인 수보다 stk의 가장 위에 있는 수(top)가 더 크면 pop
@@ -54,18 +54,19 @@ int main()
             stk.pop();
         }
 
-        //위 과정이 끝났을 때의 stk의 top을 오등큰수로 저장
+        // 위 과정이 끝났을 때의 stk의 top을 오등큰수로 저장
         ans.push(stk.top());
 
-        //현재 스캔 중인 수를 스택에 저장
+        // 현재 스캔 중인 수를 스택에 저장
         stk.push(seq.top());
 
-        //다음 수 스캔
+        // 다음 수 스캔
         seq.pop();
     }
 
-    while(!ans.empty()){
-        cout<<ans.top()<<' ';
+    while (!ans.empty())
+    {
+        cout << ans.top() << ' ';
         ans.pop();
     }
 
