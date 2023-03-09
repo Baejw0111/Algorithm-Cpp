dc5146febfd6dad77cd2f306556b7be3064b6fdf
Baejw0111 <87963766+Baejw0111@users.noreply.github.com>
2023-03-09 09:00:48 +0900

    Modify BOJ 1934

--- BOJ/백준 알고리즘 강의/알고리즘 기초 1/2. 수학/300(수학 1)/1934(최소공배수).cpp
+++ BOJ/백준 알고리즘 강의/알고리즘 기초 1/2. 수학/300(수학 1)/1934(최소공배수).cpp
@@ -1,33 +1,32 @@
-#include <iostream>
-#include <string>
+// https://www.acmicpc.net/problem/1934
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a,int b){
-    if(a%b==0){
-        return b;
-    }
-    else{
-        return gcd(b,a%b);
-    }
+int gcd(int a, int b) {
+  if (a % b == 0) {
+    return b;
+  } else {
+    return gcd(b, a % b);
+  }
 }
 
-int main(){
-    int n;
-    cin>>n;
+int main() {
+  int n;
+  cin >> n;
 
-    for(int i=0;i<n;i++){
-        int a,b;
-        cin>>a>>b;
+  for (int i = 0; i < n; i++) {
+    int a, b;
+    cin >> a >> b;
 
-        if(a<b){
-            int tmp=a;
-            a=b;
-            b=tmp;
-        }
-
-        cout<<a*b/gcd(a,b)<<endl;
+    if (a < b) {
+      int tmp = a;
+      a = b;
+      b = tmp;
     }
 
+    cout << a * b / gcd(a, b) << endl;
+  }
 
-    return 0;
+  return 0;
 }
